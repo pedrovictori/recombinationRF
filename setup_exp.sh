4029df912736babe3282a6a46c4a81334cf7b465
Pedro Victori <fosforilasa@gmail.com>
2017-08-17 13:28:39 +0100

    bash script for setting up a new experiment

---
+++ setup_exp.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+mkdir $1
+mkdir $1/fits
+mkdir $1/ACCplots
+mkdir $1/ROCplots
+mkdir $1/varImpPlots
+cp parallelPredictionPerformance.R $1
+cp parallelRandomForest.R $1
+cp $2 $1
+cp $3 $1
+
