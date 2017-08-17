06e87dc7f36ad5e5c6108ade80f1c0aeb1692f79
Pedro Victori <fosforilasa@gmail.com>
2017-08-17 14:48:37 +0100

    add help option and allow to specify the random forest script that should be used

--- setup_exp.sh
+++ setup_exp.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
-
+if [ "$1" == "-h" ]; then
+  echo "Usage: setup_exp.sh [experiment folder name] [training data file] [validation data file] [random forest script]"
+  exit 0
+fi
 mkdir $1
 mkdir $1/fits
 mkdir $1/ACCplots
 mkdir $1/ROCplots
 mkdir $1/varImpPlots
 cp parallelPredictionPerformance.R $1
-cp parallelRandomForest.R $1
 cp .Renviron $1
 cp .Rprofile $1
 cp $2 $1
 cp $3 $1
+cp $4 $1
 
