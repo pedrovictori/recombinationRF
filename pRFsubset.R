aae432830e667f92b6f1e9816eb6c1531dff4d20
Pedro Victori <fosforilasa@gmail.com>
2017-08-21 17:26:25 +0100

    make pairs of subTraining and validation data with parallel execution

---
+++ pRFsubset.R
@@ -0,0 +1,58 @@
+library(readr)
+library(RPushbullet)
+
+options(error = function() { 
+  pbPost("note", "Error", geterrmessage())
+})
+
+#default
+nIter = 20
+
+
+#parameter parsing
+args = commandArgs(trailingOnly = TRUE)
+if(length(args)<=1 && is.numeric(args[1])){
+  nIter = args[1]
+} else{
+  print("bad argument input, exiting")
+  quit(save="no")
+}
+
+
+startTime = Sys.time()
+print(paste("Script pRFsubset started executing at: ", startTime))
+
+training <- read_csv("training.csv")
+#folder for storing the subsets 
+path = "./subsets"
+file.fits = dir(path, pattern = ".RData")
+nFiles = length(file.fits)
+#setup parallel backend
+cores = detectCores()
+nCores = cores[1]-1  #not to overload computer
+
+if(nFiles<nCores){ #only need one core per iteration
+  nCores = nFiles
+}
+
+cl <- makeCluster(nCores)
+
+registerDoParallel(cl)
+cat("\ncluster set")
+
+foreach(i=1:nIter,.packages = 'randomForest') %dopar% {
+  #split training in two other dataframes, 80% for training and 20% for validation
+  get80 = floor(nrow(training) * .8)
+  get20 = nrow(training) - get80
+  subTraining = training[sample(nrow(training), get80), ]
+  validation = training[sample(nrow(training), get20), ]
+  write_csv(subTraining, paste(path,"/subTraining", i, ".csv",sep=""))
+  write_csv(validation, paste(path,"/validation", i, ".csv",sep=""))
+}
+
+#print time and total execution time, send Pushbullet notification
+endTime = Sys.time()
+execTime = endTime - startTime
+msg = paste("Script rfSubset1 finished executing at: ", endTime, "and took ", execTime, " seconds")
+print(msg)
+pbPost("note", "execution finished", msg)
