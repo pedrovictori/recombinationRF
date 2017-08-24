2f9d9198a832e397a484a65a6d858fdbd429f19a
Pedro Victori <fosforilasa@gmail.com>
2017-08-24 15:29:15 +0100

    storing all history.csv and selectedVars.csv files in a separate folder

--- archive_varsel_exp.sh
+++ archive_varsel_exp.sh
@@ -4,9 +4,12 @@ mkdir $1
 mkdir $1/ACCplots
 mkdir $1/ROCplots
 mkdir $1/varImpPlots
+mkdir $1/varSelHistory
+
 mv ACCplots/*.png $1/ACCplots/
 mv ROCplots/*.png $1/ROCplots/
 mv varImpPlots/*.png $1/varImpPlots/
+mv varSelHistory/*.csv $1/varSelHistory/
 
 mv firstRocAll.png $1/
 mv selectedRocAll.png $1/
@@ -15,5 +18,3 @@ mv firstAccAll.png $1/
 mv selectedAccAll.png $1/
 mv firstForestResults.csv $1/
 mv selectedForestResults.csv $1/
-mv history.csv $1/
-mv selectedVars.csv $1/
